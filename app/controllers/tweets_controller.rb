6eeba34e73e699bd8dc625b22c4eb47db57507a4
Eddie Moon <eddiemoonie@gmail.com>
2018-07-16 16:38:08 +0000

    automatically backed up by learn

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,31 @@
 class TweetsController < ApplicationController
 
+  get '/tweets' do
+    if logged_in?
+      @tweets = Tweet.all
+      erb :'tweets/tweets'
+    else
+      redirect '/login'
+    end
+    # @user = User.find(session[:user_id])
+    # @tweets = Tweet.find_by(session[:user_id])
+    # erb :'tweets/tweets'
+  end
 
+  post '/tweets' do
+    if logged_in?
+      if params[:content] == ""
+        redirect "/tweets/new"
+      else
+        @tweet = current_user.tweets.build(:content => params[:content])
+        if @tweet.save
+          redirect "/tweets/#{@tweet.id}"
+        else
+          redirect "/tweets/new"
+        end
+      end
+    else
+      redirect '/login'
+    end
+  end
 end
