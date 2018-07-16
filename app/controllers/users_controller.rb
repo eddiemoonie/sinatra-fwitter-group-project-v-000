57d92cf2be685a6e2caa91e81667144ed054b504
Eddie Moon <eddiemoonie@gmail.com>
2018-07-16 19:14:22 +0000

    automatically backed up by learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -44,7 +44,7 @@ class UsersController < ApplicationController
   get '/logout' do
     if logged_in?
       session.destroy
-      redirect '/tweets'
+      redirect '/login'
     else
       redirect '/'
     end
